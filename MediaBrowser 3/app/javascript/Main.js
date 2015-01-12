bf5b65b304af42b6e4220316250f4a28c6cd49bb
im85288 <immclaughlin@hotmail.com>
2015-01-12 20:36:02 +0000

    update to v0.521

--- MediaBrowser 3/app/javascript/Main.js
+++ MediaBrowser 3/app/javascript/Main.js
@@ -4,7 +4,7 @@ var tvKey = new Common.API.TVKeyValue();
 
 var Main =
 {
-		version : "v0.520",
+		version : "v0.521",
 		requiredServerVersion : "3.0.5211",
 		requiredDevServerVersion : "3.0.5211",
 		

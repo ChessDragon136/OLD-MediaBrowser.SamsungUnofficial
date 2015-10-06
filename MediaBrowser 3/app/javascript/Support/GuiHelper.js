cc126815a5bd5f790a3505acd65e23c5ab3871e3
cmcgerty <cmcgerty@users.noreply.github.com>
2015-10-06 20:49:11 +0100

    Broke the screensaver in the last commit.
    Whoops

--- MediaBrowser 3/app/javascript/Support/GuiHelper.js
+++ MediaBrowser 3/app/javascript/Support/GuiHelper.js
@@ -21,19 +21,33 @@ GuiHelper.toggleHelp = function(helpPage) {
 }
 
 GuiHelper.keyDown = function() {
-	widgetAPI.blockNavigation(event);
-	if (document.getElementById("GuiImagePlayer_ScreensaverOverlay").style.visibility == "hidden"){
-		document.getElementById("GuiImagePlayer_ScreensaverOverlay").style.visibility = "";
+	var keyCode;
+	if (event) {
+	 keyCode = event.keyCode;	
+	} else {
+		return;
 	}
-		
 	
-	//If required for Screensaver call in GuiImagePlayer_Screensaver
-	if (document.getElementById("Help").style.visibility == "") {
-		//Hide help window
-		document.getElementById("Help").style.visibility = "hidden";
-		
-		//Set focus back to original page
-		document.getElementById(this.helpPage).focus();
+	switch(keyCode) {
+		case tvKey.KEY_RED:
+		case tvKey.KEY_RETURN:
+			
+			//Stops Return causing the app to exit when closing help text.
+			widgetAPI.blockNavigation(event);
+			
+			if (document.getElementById("GuiImagePlayer_ScreensaverOverlay").style.visibility == "hidden"){
+				document.getElementById("GuiImagePlayer_ScreensaverOverlay").style.visibility = "";
+			}
+			
+			//If required for Screensaver call in GuiImagePlayer_Screensaver
+			if (document.getElementById("Help").style.visibility == "") {
+				//Hide help window
+				document.getElementById("Help").style.visibility = "hidden";
+				
+				//Set focus back to original page
+				document.getElementById(this.helpPage).focus();
+			}
+			break;
 	}
 }
 
@@ -66,7 +80,6 @@ GuiHelper.generateDisplayOneItemHelp = function() {
 		"Green - Mark photo as a favourite. <br>" +
 		"Yellow - Toggle Date/Time overlay; None | Date | Date : Time <br>" +
 		"Blue - When music is playing, show the music player controls. ";
-		return htmlToAdd;
 		} else {
 		var htmlToAdd = "Tools - Show the main menu. <br> " +
 		"Return - Returns the user to the previous screen. <br> " +
@@ -77,8 +90,8 @@ GuiHelper.generateDisplayOneItemHelp = function() {
 		"Yellow - Add or remove an item on the favourites page. <br>" +
 		"Blue - When music is playing, show the music player controls. <br>" +
 		"Ch. Up/Down - Skips one page of content. ";
-		return htmlToAdd;	
 	}
+	return htmlToAdd;
 	
 }
 
